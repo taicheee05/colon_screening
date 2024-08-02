b729983aeb4c5fccdff3f580c8d1ebff5041fcd1
taicheee05 <kyokui150101@gmail.com>
2024-08-02 16:52:18 +0900

    file update

---
+++ install_package.py
@@ -0,0 +1,9 @@
+from collections import namedtuple
+import pydotplus
+from IPython.display import Image
+from sklearn import tree
+from collections import namedtuple
+import graphviz
+import pydot
+from IPython.display import Image
+import numpy as np
