1c4b789fe70c8d90ac048ccb56de270975b0fb20
taicheee05 <kyokui150101@gmail.com>
2024-08-08 17:45:46 +0900

    update

--- .ipynb_checkpoints/data-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd  # pandasをインポート
-# 遷移確率のCSVファイルを読み込む
-change_probability = pd.read_csv('senni_pro.csv')
-variable = pd.read_csv('Variable.csv')
-
-print(change_probability)
-print(variable)
-
